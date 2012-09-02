5e01e21c2f415a0856812b94ae1cbd20f94e0174
Victor Tangermann <v.tanger@gmail.com>
2012-09-02 18:18:03 -0400

    Finalizing home.html.erb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarketFour::Application.config.secret_token = 'f03517f93fc442432eb24cb8f250d1742070aaca15b6389c3bc374b6dfe7221b675dee61cdfca6723a39291f4f7b19ec79020106bc0e8420332314bb7d3cd65f'
